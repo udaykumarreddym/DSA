11ef50ab9cbb6ad7a8e215d6d371fe6fae61c2b6
UDAY KUMAR REDDY <udaykumarreddy@UDAYs-MacBook-Air.local>
2024-08-17 11:52:28 +0530

    update

--- Array.java
+++ Array.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 public class Array{
     public static void main(String[] args){
-        //selectionsort();
-        //bubblesort();
-        //insertionsort();
-        //mergeSort();
-        //quicksort();
         //sortedArray();
         //removeduplicates();
         //rotateLeftK();
@@ -16,221 +11,10 @@ public class Array{
         //mooreVoting();
         //dutchNationalFlag();
         //kadanes();
-        nextPermutation();
+        //nextPermutation();
+        //binarySearch();
+        lowerbound();
     }
-
-
-    public static void bubblesort(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array size:");
-        int n = sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter elements:");
-        for(int i=0;i<arr.length;i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Initial array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        for(int i=arr.length-1;i>=1;i--){
-            int didSwap =0;
-            for(int j=0;j<=i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    swap(arr,j,j+1);
-                    didSwap = 1;
-                }
-            }
-            if(didSwap==0){
-                break;
-            }
-        }
-        System.out.println("\nSorted array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-
-
-    public static void selectionsort(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array size:");
-        int n = sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter elements:");
-        for(int i=0;i<arr.length;i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Initial array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        for(int i=0;i<=n-2;i++){
-            int min = i;
-            for(int j=i;j<=n-1;j++){
-                if(arr[j]<arr[min]){
-                    min = j;
-                }
-            }
-            swap(arr,i,min);
-        }
-        System.out.println("\nSorted array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-
-
-    public static void insertionsort(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array size:");
-        int n = sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter elements:");
-        for(int i=0;i<arr.length;i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Initial array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        for(int i=0;i<arr.length;i++){
-            int j = i;
-            while(j>0 && arr[j-1]>arr[j]){
-                swap(arr,j-1,j);
-                j--;
-            }
-        }
-        System.out.println("\nSorted array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-
-
-    public static void swap(int[] arr,int a,int b){
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
-
-
-    public static void mergeSort(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array size:");
-        int n = sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter elements:");
-        for(int i=0;i<arr.length;i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Initial array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-
-        mergeSort1(arr,0,arr.length-1);
-        
-        System.out.println("\nSorted array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-    public static void mergeSort1(int[] arr,int l,int r){
-        if(l==r){
-            return;
-        }
-        int m = (l+r)/2;
-        mergeSort1(arr,l,m);
-        mergeSort1(arr,m+1,r);
-        merge(arr,l,m,r);
-        
-    }
-    public static void merge(int[] arr, int l, int m, int r){
-        
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
-        
-        System.arraycopy(arr, l, leftArray, 0, n1);
-        System.arraycopy(arr, m + 1, rightArray, 0, n2);
-        
-        int i = 0, j = 0;
-        int k = l;
-
-        while (i < n1 && j < n2) {
-            if (leftArray[i] <= rightArray[j]) {
-                arr[k] = leftArray[i];
-                i++;
-            } else {
-                arr[k] = rightArray[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < n1) {
-            arr[k] = leftArray[i];
-            i++;
-            k++;
-        }
-        
-        while (j < n2) {
-            arr[k] = rightArray[j];
-            j++;
-            k++;
-        }
-    }
-    public static void quicksort(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array size:");
-        int n = sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter elements:");
-        for(int i=0;i<arr.length;i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Initial array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-
-        quickSort1(arr,0,arr.length-1);
-
-        System.out.println("\nSorted array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-    public static void quickSort1(int[] arr, int low, int high){
-        if(low<high){
-            int pIndex = partition(arr,low,high);
-            quickSort1(arr, low, pIndex-1);
-            quickSort1(arr, pIndex+1, high);
-        }
-    }
-    public static int partition(int[] arr,int low,int high){
-        int pivot = arr[low];
-        int i = low;
-        int j = high;
-        while(i<j){
-            while(arr[i] <= pivot && i <= high-1){
-                i++;
-            }
-            while(arr[j] > pivot && j >= low){
-                j--;
-            }
-            if(i<j){
-                swap(arr,i,j);
-            }
-        }
-        swap(arr,low,j);
-        return j;
-    }
-
-
     public static void sortedArray(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter array size:");
@@ -550,5 +334,63 @@ public class Array{
                 System.out.print(num+" ");
             }
         }
+    }
+
+    public static void binarySearch(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target:");
+        int target = sc.nextInt();
+        int low = 0, high = n-1;
+        int flag = 0;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(arr[mid]==target){
+                flag = 1;
+                System.out.println("Target found at "+mid+" position");
+                break;
+            }
+            else if(target > arr[mid]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        if(flag == 0){
+            System.out.println("Target not found");
+        }
+    }
+
+    public static void lowerbound(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target:");
+        int target = sc.nextInt();
+        int ans = n;
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid]>=target){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        System.out.println("Lower bound found at "+ans+" position");
     }
 }
---
+++ Sorting.java
@@ -0,0 +1,220 @@
+import java.util.Scanner;
+
+public class Sorting {
+    public static void main(String[] args) {
+        //selectionsort();
+        //bubblesort();
+        //insertionsort();
+        //mergeSort();
+        quicksort();
+    }
+    public static void bubblesort(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Initial array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        for(int i=arr.length-1;i>=1;i--){
+            int didSwap =0;
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                    didSwap = 1;
+                }
+            }
+            if(didSwap==0){
+                break;
+            }
+        }
+        System.out.println("\nSorted array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    public static void selectionsort(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Initial array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        for(int i=0;i<=n-2;i++){
+            int min = i;
+            for(int j=i;j<=n-1;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            swap(arr,i,min);
+        }
+        System.out.println("\nSorted array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    public static void insertionsort(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Initial array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        for(int i=0;i<arr.length;i++){
+            int j = i;
+            while(j>0 && arr[j-1]>arr[j]){
+                swap(arr,j-1,j);
+                j--;
+            }
+        }
+        System.out.println("\nSorted array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    public static void swap(int[] arr,int a,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    public static void mergeSort(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Initial array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        mergeSort1(arr,0,arr.length-1);
+        
+        System.out.println("\nSorted array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void mergeSort1(int[] arr,int l,int r){
+        if(l==r){
+            return;
+        }
+        int m = (l+r)/2;
+        mergeSort1(arr,l,m);
+        mergeSort1(arr,m+1,r);
+        merge(arr,l,m,r);
+        
+    }
+    public static void merge(int[] arr, int l, int m, int r){
+        
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+        
+        System.arraycopy(arr, l, leftArray, 0, n1);
+        System.arraycopy(arr, m + 1, rightArray, 0, n2);
+        
+        int i = 0, j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+    public static void quicksort(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Initial array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        quickSort1(arr,0,arr.length-1);
+
+        System.out.println("\nSorted array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void quickSort1(int[] arr, int low, int high){
+        if(low<high){
+            int pIndex = partition(arr,low,high);
+            quickSort1(arr, low, pIndex-1);
+            quickSort1(arr, pIndex+1, high);
+        }
+    }
+    public static int partition(int[] arr,int low,int high){
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while(i<j){
+            while(arr[i] <= pivot && i <= high-1){
+                i++;
+            }
+            while(arr[j] > pivot && j >= low){
+                j--;
+            }
+            if(i<j){
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,low,j);
+        return j;
+    }
+}
