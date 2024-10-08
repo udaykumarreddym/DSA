a530e0284cfe0dd45f85d4c9f8d25d10c6df6de3
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2024-10-08 21:06:12 +0530

    update

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,15 @@
 public class Recursion {
     public static void main(String[] args) {
-        
+        double ans = myPow(2,10);
+        System.out.println(ans);
+    }
+    public static double myPow(double x, long n) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 1 / myPow(x, -n);
+        if (n % 2 == 1)
+          return x * myPow(x, n - 1);
+        return myPow(x * x, n / 2);
     }
 }
