8ac4e92f82136ba469be5faa567a0b125e3b2183
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2024-10-10 20:55:09 +0530

    update

--- Recursion.java
+++ Recursion.java
@@ -1,6 +1,7 @@
 public class Recursion {
     public static void main(String[] args) {
-        double ans = myPow(2,10);
+        //double ans = myPow(2,10);
+        boolean ans = pow2(1);
         System.out.println(ans);
     }
     public static double myPow(double x, long n) {
@@ -11,5 +12,17 @@ public class Recursion {
         if (n % 2 == 1)
           return x * myPow(x, n - 1);
         return myPow(x * x, n / 2);
+    }
+    public static boolean  pow2(int n){
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 != 0) {
+            return false;
+        }
+        return pow2(n / 2);
     }
 }
